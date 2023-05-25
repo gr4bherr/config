1e80a92d3dc680352eaccbf93af6562245e6f7df
gr4bherr <gr4bherr@fastmail.com>
2023-05-25 18:44:09 +0200

    ad

--- run.sh
+++ run.sh
@@ -12,6 +12,9 @@ defaults write com.apple.finder CreateDesktop false
 # set key repeat on hold
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# disable disk not ejected propely message
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
+
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
