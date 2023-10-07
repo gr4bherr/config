932014ef13de5260957f98fcf64ac026c03ca953
Adam Grabherr <gr4bherr@macbook.local>
2023-10-07 07:49:24 +0200

    new packages

--- .zshrc
+++ .zshrc
@@ -1,3 +1,4 @@
 PS1="%n %~ %# "
 
 export PATH=/usr/local/bin:$PATH
+export PATH=/opt/homebrew/bin:$PATH
--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# move run commands
+mv .vimrc ~/
+mv .zshrc ~/
+mv .tmux.conf ~/
+
 # dont show last login in terminal
-touch .hushlogin
+touch ~/.hushlogin
 
 # allow applications downloaded from anywhere
 sudo spctl --master-disable
@@ -18,9 +23,11 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # disable disk not ejected propely message
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
 
-
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install brew packages
 brew install $(cat packages)
+
+# analytics off
+brew analytics off
