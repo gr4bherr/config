936a8ff697d23a0dbf333f3ffb7ee044995e529c
Adam Grabherr <gr4bherr@macbook.local>
2023-10-07 10:40:05 +0200

    lil edit

--- run.sh
+++ run.sh
@@ -24,10 +24,10 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
 
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install brew packages
-brew install $(cat packages)
+cat packages | xargs brew install
 
 # analytics off
 brew analytics off
