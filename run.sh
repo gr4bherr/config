c5f3e89029faaca4515bfb0ee601b49ee8ec2d02
Adam Grabherr <gr4bherr@macbook.local>
2023-10-31 07:28:28 +0100

    add echo, dock show delay

--- run.sh
+++ run.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-# move run commands
+echo "move .vimrc to ~/"
 mv .vimrc ~/
+echo "zshrc .vimrc to ~/"
 mv .zshrc ~/
+echo ".tmux to ~/"
 mv .tmux.conf ~/
 
-# dont show last login in terminal
+echo "dont show last login in terminal"
 touch ~/.hushlogin
 
-# allow applications downloaded from anywhere
+echo "allow applications downloaded from anywhere"
 sudo spctl --master-disable
 
-# dont show icons on desktop
+echo "disable dock show delay"
+defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
+
+echo "dont show icons on desktop"
 defaults write com.apple.finder CreateDesktop false
 
-# dont play chime when charger plugged in 
+echo "dont play chime when charger plugged in"
 defaults write com.apple.PowerChime ChimeOnNoHardware -bool true; killall PowerChime
 
-# set key repeat on hold
+echo "set key repeat on hold"
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# disable disk not ejected propely message
+echo "disable disk not ejected propely message"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
 
-# install homebrew
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# install brew packages
-cat packages | xargs brew install
-
-# analytics off
+echo "analytics off"
 brew analytics off
